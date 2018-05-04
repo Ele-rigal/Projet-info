2ba3e1bf5b54bd602d44e54f967505a51bec7a21
Ele-rigal <riwgal2@gmail.com>
2018-05-04 13:35:16 +0200

    Update génération_man.c

--- génération_man.c
+++ génération_man.c
@@ -1,267 +1,207 @@
 #include <stdlib.h>
+
 #include <stdio.h>
+
 #include <time.h>
+
 /* pour compiler ce fichier:  gcc –o cocotte cocotte.c libgraphique.c –lglut –lGLU –lGL –lm */
-
-
-
-
-
-
-
 /* indispensable pour compiler un programme utilisant la bibliothèque graphique: */
-
 #include "libgraphique.h"
-
 #include "libgraphique_fonts.h"
-
 #include "libgraphique.c"
-
 #include <unistd.h>
-
-#include <stdio.h>
-
+//#include <stdio.h>
 #ifdef __unix__
-
 # include <unistd.h>
-
 #elif defined _WIN32
-
 # include <windows.h>
-
 #define sleep(x) Sleep(1000 * x)
-
 #endif
+#define COTE_MIN (WINDOW_WIDTH < WINDOW_HEIGHT ? WINDOW_WIDTH : WINDOW_HEIGHT)
+typedef int ENTIER;
+ENTIER x;
+void ecrit_init(FILE* fic,int n,int t,int co,int li,int finco,int finli)
+{
+	fprintf(fic,"%d \n",n);
+	fprintf(fic,"%d \n",t);
+	fprintf(fic,"%d \n",co);
+	fprintf(fic,"%d \n",li);
+	fprintf(fic,"%d \n",finco);
+	fprintf(fic,"%d \n",finli);
+}
+void ecrit(FILE* fic,int x1,int y1,int x2,int y2)
+{
+	fprintf(fic,"%d \n",x1);
+	fprintf(fic,"%d\n",y1);
+	fprintf(fic,"%d\n",x2);
+	fprintf(fic,"%d\n",y2);
+}
+void sup_g(FILE* fichier,int li,int co,int t)
+{//supprime cloison quand on tourne a gauche
+    int x1=li-(t/2);
+    int y1=co-(t/2);
+    int x2=li-(t/2);
+    int y2=co+(t/2);
+    ecrit(fichier,x1,y1,x2,y2);
+    set_drawing_color(color_WHITE);
+    draw_line(x1,y1,x2,y2);
+    update_graphics();
+}
+void sup_d(FILE* fichier,int li,int co,int t)
+{//supprime cloison quand on tourne a droite
+    int x1=li+(t/2);
+    int y1=co-(t/2);
+    int x2=li+(t/2);
+    int y2=co+(t/2);
+     ecrit(fichier,x1,y1,x2,y2);
+    set_drawing_color(color_WHITE);
+    draw_line(x1,y1,x2,y2); update_graphics();
+}
+void sup_haut(FILE* fichier,int li,int co,int t)
+{   int x1=li-(t/2);
+    int y1=co+(t/2);
+    int x2=li+(t/2);
+    int y2=co+(t/2);
+     ecrit(fichier,x1,y1,x2,y2);
+    set_drawing_color(color_WHITE);
+    draw_line(x1,y1,x2,y2);
+    update_graphics();
+}
 
-#include "lib.h"
+void sup_bas(FILE* fichier,int li,int co,int t)
+{	int x1=li-(t/2);
+	int y1=co-(t/2);
+	int x2=li+(t/2);
+	int y2= co-(t/2);
+	 ecrit(fichier,x1,y1,x2,y2);
+    set_drawing_color(color_WHITE);
+    draw_line(x1, y1, x2, y2);
+    update_graphics();
+}
+
+long factorielle(int n)
+{
+   if(n == 0)
+      return 1;
+   else
+      return n*factorielle(n-1);
+}
+
 
 int main(void){
+char rep[100];
+    srand(time(NULL)); //init la fonction rand()
+    int i,n,m,t,xn1,yn1,xn2,yn2,xm1,ym1,xm2,ym2;
+	printf("entrez le nom du fic à creer(format .txt):   ");
+	scanf("%s",rep);
 
-
-
-	int i,n,m,t,xn1,yn1,xn2,yn2,xm1,ym1,xm2,ym2;
-
-
-
-	printf("entrez la taille du cote du carre:");
-
-
-
-	scanf("%d",&n);
-
-
-
-	m=n;
-
-	printf("entrez la taille des cases:");
-
-
-
-	scanf("%d",&t);
+//crea fichier
+	FILE* fichier;
+	fichier=fopen(rep,"w");
+    printf("entrez la taille du cote du carre:");
+    scanf("%d",&n);
+    m=n;
+    printf("entrez la taille des cases:");
+    scanf("%d",&t);
 
 //calcul pour centrer la grille
-
-	int marge_c=(WINDOW_WIDTH-(t*n))/2;
-
-
-
-	int marge_h=(WINDOW_HEIGHT-(t*m))/2;
-
-char index(char c,int x)
-
-{
-
-    c=x-'0';
-
-    return c;
-
-}
-	
-ini_graph();
-//initialisation procédures graphiques
-
+int marge_c=(WINDOW_WIDTH-(t*n))/2;
+int marge_h=(WINDOW_HEIGHT-(t*m))/2;
+//début de l'interface graphique
+start_graphics();
+set_drawing_color(color_BLACK);
 //coordonnées des marges à prendre pour centrer
-
-	xn1=marge_c;
-
-
-
-	yn1=marge_h;
-
-
-
-	xm1=marge_c;
-
-
-
-	ym1=marge_h;
-
-
-
-
-
-	for(i=1;i<=n+1;i++){
-
+xn1=marge_c;
+yn1=marge_h;
+xm1=marge_c;
+ym1=marge_h;
+for(i=1;i<=n+1;i++){
 //dessin des lignes
-
-		xn2=xn1+(m)*t;
-
-		yn2=yn1;
-
-		draw_line(xn1, yn1, xn2, yn2);
-
+    xn2=xn1+(m)*t;
+    yn2=yn1;
+    draw_line(xn1, yn1, xn2, yn2);
 		/*if (i<=n){
-
             char *c=i-'0';
-
             draw_string(xn1-t,yn1+(t/2),&c);
-
-		}*/
-
-		yn1=yn1+t;
-
-
-
-	}
-
-	for(i=1;i<=m+1;i++){
-
-//dessin des colonnes
-
-		ym2=ym1+(n)*t;
-
-		xm2=xm1;
-
-		draw_line(xm1, ym1, xm2, ym2);
-
-		/*if (i<=m){
-
-            draw_string(xm1+(t/2),ym1-t,&i);
-
-		}*/
-
-		xm1=xm1+t;
-
-
-
-	}
-
-//j'ai cherché ici les coordonnees de debut et de fin
-
-//draw_circle_full((xn1+(t/2)),(yn1-(1.5*t)),5);
-
-//draw_circle_full((xm1-(1.5*t)),(ym1+(t/2)),5);
-
-int li=xn1+(t/2);
-
-int co=yn1-(1.5*t);
-
-int finli=xm1-(1.5*t);
-
-int finco=ym1+(t/2);update_graphics();
-
-draw_circle_full(li,co,7);
-
-update_graphics();
-
-sup_g(li,co,t);
-//int y=dir();
-while((li<=finli)&&(co>=finco))
-
-{   int touche=get_key();
-
-    if(/*(y==0)*/(touche==key_LEFT)&&(li>(xn1+(t/2))))
-
-    {   li=li-t;
-
-        draw_circle_full(li,co,7);
-
-        sup_g(li+t,co,t);
-
-        draw_circle_full(li+t,co,7);
-
-        set_drawing_color(color_BLACK);
-        //y=dir();
-
-    }
-
-    else if(/*(y==1)*/(touche==key_RIGHT)&&(li<xn2-(t/2)))
-
-    {   li=li+t;
-
-        draw_circle_full(li,co,7);
-
-        sup_d(li-t,co,t);
-
-        draw_circle_full(li-t,co,7);
-
-        set_drawing_color(color_BLACK);
-        //y=dir();
-
-    }
-
-    else if(/*(y==2)*/(touche==key_DOWN)&&(co>finco))
-
-    {   co=co-t;
-
-        draw_circle_full(li,co,7);
-
-        sup_bas(li,co+t,t);
-
-        draw_circle_full(li,co+t,7);
-
-        set_drawing_color(color_BLACK);
-        //y=dir();
-
-    }
-
-    else if(/*(y==3)*/(touche==key_UP)&&(co<yn1-(1.5*t)))
-
-    {   co=co+t;
-
-        draw_circle_full(li,co,7);
-
-        sup_haut(li,co-t,t);
-
-        draw_circle_full(li,co-t,7);
-
-        set_drawing_color(color_BLACK);
-        //y=dir();
-
-    }
-
-    else if (/*(y==1)*/(touche==key_RIGHT)&&((li==finli)&& (co==finco)))
-
-    {   li=li+t;
-
-        draw_circle_full(li,co,7);
-
-        sup_d(li-t,co,t);
-
-        draw_circle_full(li-t,co,7);
-
-        set_drawing_color(color_BLACK);
-
-        sleep(20);
-
-        stop_graphics();
-
-    }
-
-
-
-     update_graphics();
-
-
-
+        } essai de l'index mais ne fonctionne pas*/
+    yn1=yn1+t;
 }
+for(i=1;i<=m+1;i++){
+//dessin des colonnes
+    ym2=ym1+(n)*t;
+    xm2=xm1;
+    draw_line(xm1, ym1, xm2, ym2);
+    /*if (i<=m){
+        draw_string(xm1+(t/2),ym1-t,&i);
+    }*/
+    xm1=xm1+t;
+}
+//coordonnées du curseur départ
+int li=xn1+(t/2);
+int co=yn1-(1.5*t);
+//coordonnées de la fin
+int finli=xm1-(1.5*t);
+int finco=ym1+(t/2);update_graphics();
+int r=t/6; //rayon
+draw_circle_full(li,co,r); //création du curseur
+update_graphics();
+int touche;
+int g=factorielle(n)-n*n;
+int d=factorielle(n)-n*n;
+int h=factorielle(n)-n*n;
+int b=factorielle(n)-n*n;
+set_drawing_color(color_BLACK);
+ecrit_init(fichier,n,t,co,li,finco,finli);
+sup_g(fichier,li,co,t);
+while((li<=finli)&&(co>=finco))
+{
+touche=get_key();
+//printf("%d",touche); mouchar pour vérifier que le rand marchait bien
+    if(((touche==key_LEFT)&&(li>(xn1+(t/2))))&&(g>0))
+    {   li=li-t;
+        draw_circle_full(li,co,r);
+        sup_g(fichier,li+t,co,t);
+        draw_circle_full(li+t,co,r);
+        set_drawing_color(color_BLACK);
+        g--;
 
-
-
-
-
-	return 0;
-
-
-
+    }
+    else if(((touche==key_RIGHT)&&(li<xn2-(t/2)))&&(d>1))
+    {   li=li+t;
+        draw_circle_full(li,co,r);
+        sup_d(fichier,li-t,co,t);
+        draw_circle_full(li-t,co,r);
+        set_drawing_color(color_BLACK);
+        d--;
+    }
+    else if(((touche==key_DOWN)&&(co>finco))&&(b>1))
+    {   co=co-t;
+        draw_circle_full(li,co,r);
+        sup_bas(fichier,li,co+t,t);
+        draw_circle_full(li,co+t,r);
+        set_drawing_color(color_BLACK);
+        b--;
+    }
+    else if(((touche==key_UP)&&(co<yn1-(1.5*t)))&&(h>0))
+    {   co=co+t;
+        draw_circle_full(li,co,r);
+        sup_haut(fichier,li,co-t,t);
+        draw_circle_full(li,co-t,r);
+        set_drawing_color(color_BLACK);
+        h--;
+    }
+    else if (((li==finli)&& (co==finco)))
+    {   li=li+t;
+        draw_circle_full(li,co,r);
+        sup_d(fichier,li-t,co,t);
+        draw_circle_full(li-t,co,r);
+        set_drawing_color(color_BLACK);
+	fclose(fichier);
+        sleep(3);
+        stop_graphics();
+    }
+   update_graphics();
+}
+get_key();
+return 0;
 }
